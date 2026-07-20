e6cb67cd886d512ece8ec2896a9adec048379030
ryanmeador1 <ryanmeador1@gmail.com>
2026-07-20 13:54:25 -0400

    Update CSV file path in project_app.py

--- project_app.py
+++ project_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-project_df = pd.read_csv('/content/cps_project_data.csv')
+project_df = pd.read_csv('cps_project_data.csv')
 
 st.title('INFO 450 Final Project - Ryan Meador')
 
