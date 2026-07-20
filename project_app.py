578ce39861ba1237fb23a071ec63dddc0bb067c6
ryanmeador1 <ryanmeador1@gmail.com>
2026-07-20 15:38:41 -0400

    Update project_app.py

--- project_app.py
+++ project_app.py
@@ -3,6 +3,13 @@ import pandas as pd
 import plotly.express as px
 
 project_df = pd.read_csv('cps_project_data.csv')
+project_df['EDUC_GROUP'] = 'No Diploma'
+project_df.loc[project_df['EDUC'] == 73, 'EDUC_GROUP'] = 'High School'
+project_df.loc[project_df['EDUC'] == 81, 'EDUC_GROUP'] = 'Some College'
+project_df.loc[project_df['EDUC'] == 91, 'EDUC_GROUP'] = 'Associate Degree'
+project_df.loc[project_df['EDUC'] == 92, 'EDUC_GROUP'] = 'Associate Degree'
+project_df.loc[project_df['EDUC'] == 111, 'EDUC_GROUP'] = 'Bachelors Degree'
+project_df.loc[project_df['EDUC'] >= 123, 'EDUC_GROUP'] = 'Graduate Degree'
 
 st.title('INFO 450 Final Project - Ryan Meador')
 
@@ -12,3 +19,5 @@ project_df_year = project_df.loc[project_df['YEAR']==selected_year]
 result = project_df_year['EARNWEEK2'].median()
 
 st.write(f'Median Weekly Pay: ${result:.2f} for year {selected_year}')
+
+px.bar(mean_pay_educ,x='EDUC_GROUP',y='EARNWEEK2',title='Mean Weekly Pay')
