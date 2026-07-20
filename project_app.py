a0e292873f546d5169795e579a19e5649a1f4921
ryanmeador1 <ryanmeador1@gmail.com>
2026-07-20 14:30:39 -0400

    Update project_app.py

--- project_app.py
+++ project_app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -7,19 +6,8 @@ project_df = pd.read_csv('cps_project_data.csv')
 
 st.title('INFO 450 Final Project - Ryan Meador')
 
-# Creates a new column to categorize the codes for education
-project_df['EDUC_GROUP'] = 'No Diploma'
-project_df.loc[project_df['EDUC'] == 73, 'EDUC_GROUP'] = 'High School'
-project_df.loc[project_df['EDUC'] == 81, 'EDUC_GROUP'] = 'Some College'
-project_df.loc[project_df['EDUC'] == 91, 'EDUC_GROUP'] = 'Associate Degree'
-project_df.loc[project_df['EDUC'] == 92, 'EDUC_GROUP'] = 'Associate Degree'
-project_df.loc[project_df['EDUC'] == 111, 'EDUC_GROUP'] = 'Bachelors Degree'
-project_df.loc[project_df['EDUC'] >= 123, 'EDUC_GROUP'] = 'Graduate Degree'
-mean_pay_educ = project_df.groupby('EDUC_GROUP')['EARNWEEK2'].mean().sort_values(ascending=False).reset_index()
+project_df['SEX_LABEL'] = 'Male'
+project_df.loc[project_df['SEX']==2,'SEX_LABEL'] = 'Female'
 
-fig = px.bar(mean_pay_educ,x='EDUC_GROUP',y='EARNWEEK2',title='Mean Weekly Pay', labels={
-  'EDUC_GROUP':'Education Level',
-  'EARNWEEK2': 'Median Weekly Pay'
-})
-
-st.plotly_chart(fig)
+st.sidebar.header('Filter')
+filter_var = st.sidebar('Select filter',['EARNWEEK2','UHRSWORKT'])
