05590ea4122ef7041b012dd1b4f7acd47c11637a
ryanmeador1 <ryanmeador1@gmail.com>
2026-07-20 13:49:43 -0400

    Add files via upload

---
+++ project_app.py
@@ -0,0 +1,14 @@
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+project_df = pd.read_csv('/content/cps_project_data.csv')
+
+st.title('INFO 450 Final Project - Ryan Meador')
+
+mean_pay_educ = project_df.groupby('EDUC_GROUP')['EARNWEEK2'].mean().sort_values(ascending=False).reset_index()
+
+fig = px.bar(mean_pay_educ,x='EDUC_GROUP',y='EARNWEEK2',title='Mean Weekly Pay')
+
+st.plotly_chart(fig)
