a8b7f2b4a65df9378df83a456805631b45d95c75
ryanmeador1 <ryanmeador1@gmail.com>
2026-07-21 10:27:59 -0400

    Update project_app.py

--- project_app.py
+++ project_app.py
@@ -61,4 +61,4 @@ fig = px.bar(result,x='EDUC_GROUP',y='EARNWEEK2',title=f'{selected_stat} Weekly 
 st.plotly_chart(fig)
 
 fig2 = px.bar(result_sex,x='EDUC_GROUP',y=['Female','Male'],title=f'{selected_stat} Weekly Pay by Gender and Education Level',barmode='group')
-st.plotly_chart(result_sex)
+st.plotly_chart(fig2)
