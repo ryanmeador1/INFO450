cbc59a6ab8c3505600005ba27781c61dd17dd3b4
ryanmeador1 <ryanmeador1@gmail.com>
2026-07-21 10:27:22 -0400

    Fix bar chart data structure for gender comparison

--- project_app.py
+++ project_app.py
@@ -60,5 +60,5 @@ if selected_stat == 'Median':
 fig = px.bar(result,x='EDUC_GROUP',y='EARNWEEK2',title=f'{selected_stat} Weekly Pay by Education Level')
 st.plotly_chart(fig)
 
-fig2 = px.bar(result_sex,x=['EDUC_GROUP','SEX_LABEL'],y='EARNWEEK2',title=f'{selected_stat} Weekly Pay by Gender and Education Level',barmode='group')
+fig2 = px.bar(result_sex,x='EDUC_GROUP',y=['Female','Male'],title=f'{selected_stat} Weekly Pay by Gender and Education Level',barmode='group')
 st.plotly_chart(result_sex)
